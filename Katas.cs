4190d607b413d133d7553259c017a8e4db7236e7
Roman <roman.ossif256@gmail.com>
2023-06-28 15:42:25 +0200

    Solve Collatz Kata.

--- Katas.cs
+++ Katas.cs
@@ -8,7 +8,6 @@ namespace Codewars
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine(PhoneNumberKata.CreatePhoneNumber(new int[] { 1, 4, 2, 3, 5, 6, 7, 8, 9, 0 }));
         }
     }
 }
---
+++ Rank 6/CollatzKata.cs
@@ -0,0 +1,25 @@
+﻿namespace Codewars
+{
+    public class CollatzKata
+    {
+        public static string Collatz(int number)
+        {
+            if (number == 1)
+                return "1";
+
+            List<int> list = new List<int>()
+            { 
+                number
+            };
+
+            while (number != 1)
+            {
+                number = (number % 2 == 0) ? number/2 : 3*number + 1;
+                
+                list.Add(number);
+            }
+
+            return string.Join("->", list);
+        }
+    }
+}
