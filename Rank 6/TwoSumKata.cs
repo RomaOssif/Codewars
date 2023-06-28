c4594fddddbca0e500d994453fe94051b28d5b96
Roman <roman.ossif256@gmail.com>
2023-06-28 16:25:36 +0200

    Solve two sum Kata.
    The purpose of that kata was to find 2 values in a given array which would give the target value.

---
+++ Rank 6/TwoSumKata.cs
@@ -0,0 +1,28 @@
+﻿namespace Codewars
+{
+    public class TwoSumKata
+    {
+        public static int[] TwoSum(int[] numbers, int target)
+        {
+            int[] result = new int[2];
+            List<int> copy = new List<int> (numbers);
+
+            foreach(int n in copy)
+            {
+                if (copy.Contains(target - n))
+                {
+                    result[0] = copy.IndexOf(n);
+
+                    if (n == target - n)
+                        copy[result[0]] = int.MinValue;
+                    
+                    result[1] = copy.IndexOf(numbers.First(e => e == target - n));
+
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
