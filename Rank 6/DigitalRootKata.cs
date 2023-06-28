3fc2d32dce9f00ab64dd293529bbd729b538361d
Roman <roman.ossif256@gmail.com>
2023-06-28 17:27:15 +0200

    Solve digital root kata.

---
+++ Rank 6/DigitalRootKata.cs
@@ -0,0 +1,24 @@
+﻿namespace Codewars
+{
+    public class DigitalRootKata
+    {
+        public static int DigitalRoot(int n)
+        {
+            if (n <= 9)
+                return n;
+            else
+            {
+                int sum = 0;
+
+                while (n > 0)
+                {
+                    sum += n % 10;
+
+                    n /= 10;
+                }
+
+                return DigitalRoot(sum);
+            }
+        }
+    }
+}
