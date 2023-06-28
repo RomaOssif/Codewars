a3edcaae644b346d1ec2009ecf83947a56a4c2d7
Roman <roman.ossif256@gmail.com>
2023-06-28 17:06:29 +0200

    Solve binary conversion kata.

---
+++ IntoBinaryKata.cs
@@ -0,0 +1,19 @@
+﻿namespace Codewars
+{
+    class IntoBinaryKata
+    {
+        public static int binaryArrayToNumber(int[] BinaryArray)
+        {
+            double sum = 0, power = 0;
+
+            foreach (int num in BinaryArray.Reverse())
+            {
+                sum += (num == 1) ? Math.Pow(2, power) : 0;
+
+                power++;
+            }
+
+            return (int)sum;
+        }
+    }
+}
