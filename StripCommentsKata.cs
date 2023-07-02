b585ceb5e7382357ee4fb044f0f73c041efaff08
Roman <roman.ossif256@gmail.com>
2023-07-02 20:14:34 +0200

    Add first version of Strip Comment kata.

---
+++ StripCommentsKata.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Codewars
+{
+    public class StripCommentsKata
+    {
+        public static string StripComments(string text, string[] commentSymbols)
+        {
+            var result = new List<string>();
+
+            foreach (string sentence in text.Split('\n'))
+            {
+                string toAdd = sentence;
+
+                foreach (string symbol in commentSymbols)
+                {
+                    if (sentence.Contains(symbol))
+                    {
+                        toAdd = sentence.Remove(sentence.IndexOf(symbol)).Trim();
+                        break;
+                    }
+                }
+
+                result.Add(toAdd);
+            }
+
+            return string.Join('\n', result);
+        }
+    }
+}
