eff33a69d6cf282073a2df113b0e26586bf61e62
Roman <roman.ossif256@gmail.com>
2023-07-04 17:03:21 +0200

    Solve Time Format kata.

---
+++ TimeFormat.cs
@@ -0,0 +1,30 @@
+﻿namespace Codewars
+{
+    public static class TimeFormat
+    {
+        public static string GetReadableTime(int seconds)
+        {
+            int _hours = 0, _minutes = 0, _seconds = 0;
+
+            while (seconds / 3600 != 0)
+            {
+                _hours++;
+
+                seconds -= 3600;
+            }
+
+            while (seconds / 60 != 0)
+            {
+                _minutes++;
+
+                seconds -= 60;
+            }
+
+            _seconds = seconds;
+
+            return (_hours   < 10? "0" : "") + $"{_hours}:"   + 
+                   (_minutes < 10? "0" : "") + $"{_minutes}:" +
+                   (_seconds < 10? "0" : "") + $"{_seconds}";
+        }
+    }
+}
