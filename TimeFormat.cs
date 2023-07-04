70cf72dba41d4a6f25ff2b290b1ff1403c602ef9
Roman <roman.ossif256@gmail.com>
2023-07-04 17:07:06 +0200

    Improve logic in Time Format Kata.

--- TimeFormat.cs
+++ TimeFormat.cs
@@ -4,23 +4,7 @@
     {
         public static string GetReadableTime(int seconds)
         {
-            int _hours = 0, _minutes = 0, _seconds = 0;
-
-            while (seconds / 3600 != 0)
-            {
-                _hours++;
-
-                seconds -= 3600;
-            }
-
-            while (seconds / 60 != 0)
-            {
-                _minutes++;
-
-                seconds -= 60;
-            }
-
-            _seconds = seconds;
+            int _hours = seconds / 3600, _minutes = seconds / 60 % 60, _seconds = seconds % 60;
 
             return (_hours   < 10? "0" : "") + $"{_hours}:"   + 
                    (_minutes < 10? "0" : "") + $"{_minutes}:" +
