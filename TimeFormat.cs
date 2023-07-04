d80d7ab39b907b95dcb1ba7407510b147b75fcdc
Roman <roman.ossif256@gmail.com>
2023-07-04 17:21:45 +0200

    Refactor Time Format Kata.

--- TimeFormat.cs
+++ TimeFormat.cs
@@ -4,11 +4,7 @@
     {
         public static string GetReadableTime(int seconds)
         {
-            int _hours = seconds / 3600, _minutes = seconds / 60 % 60, _seconds = seconds % 60;
-
-            return (_hours   < 10? "0" : "") + $"{_hours}:"   + 
-                   (_minutes < 10? "0" : "") + $"{_minutes}:" +
-                   (_seconds < 10? "0" : "") + $"{_seconds}";
+            return string.Format("{0:00}:{1:00}:{2:00}", seconds / 3600, seconds / 60 % 60, seconds % 60);
         }
     }
 }
