5641c955af90b509855e379626919f25afd23eac
Roman <roman.ossif256@gmail.com>
2023-07-20 22:24:14 +0200

    Write 3 universal cases regardless of direction.
    These cases return the score of hitting: Bull, Bull's eye, anything outside the dashboard.

--- Rank 5/Darts.cs
+++ Rank 5/Darts.cs
@@ -4,7 +4,20 @@
     {
         public static string GetScore(double x, double y)
         {
+            double shotRadius = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+            double wholeRadius = 340;
 
+            double difference = shotRadius - wholeRadius;
+
+            if (shotRadius < 6.35)
+                return "DB";
+            if (shotRadius < 15.9)
+                return "SB";
+            if (shotRadius > 170)
+                return "X";
+
+
+            //string[] 
 
             return "";
         }
