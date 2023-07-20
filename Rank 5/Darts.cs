4c7c03bab44433976d039a27519e2535c393ac4b
Roman <roman.ossif256@gmail.com>
2023-07-20 22:06:39 +0200

    Start solving Dartboard kata.

---
+++ Rank 5/Darts.cs
@@ -0,0 +1,21 @@
+﻿namespace Codewars
+{
+    public static class Darts
+    {
+        public static string GetScore(double x, double y)
+        {
+
+
+            return "";
+        }
+    }
+}
+
+/*
+ * Outside of the board: `"X"`
+Bull's eye: `"DB"`
+Bull: `"SB"`
+A single number, example: `"10"`
+A triple number: `"T10"`
+A double number: `"D10"`
+*/
