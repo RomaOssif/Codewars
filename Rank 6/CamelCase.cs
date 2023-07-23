f073eb722003d480113b9f8db04e88e218593bc4
Roman <roman.ossif256@gmail.com>
2023-07-23 22:45:22 +0200

    Solve To camel case kata.

---
+++ Rank 6/CamelCase.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public static class CamelCase
+    {
+        public static string ToCamelCase(string str)
+        {
+            string[] words = str.Split('_', '-');
+            string camel = words[0];
+
+            foreach (string word in words.Skip(1))
+            {
+                camel += word[0].ToString().ToUpper() + word.Substring(1);
+            }
+
+            return camel;
+        }
+    }
+}
