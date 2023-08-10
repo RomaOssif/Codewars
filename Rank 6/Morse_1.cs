c3dbd4a504e056565c706c86a38d1ce2135da42f
Roman <roman.ossif256@gmail.com>
2023-08-10 22:41:41 +0200

    Solve morse code kata part 1.

---
+++ Rank 6/Morse_1.cs
@@ -0,0 +1,67 @@
+﻿namespace Codewars
+{
+    public class Morse1
+    {
+        public static string Decode(string input)
+        {
+            input = input.Trim();
+
+            var _translation = new Dictionary<string, string>()
+            {
+                [".-"] = "A",
+                ["-..."] = "B",
+                ["-.-."] = "C",
+                ["-.."] = "D",
+
+                ["."] = "E",
+                ["..-."] = "F",
+                ["--."] = "G",
+                ["...."] = "H",
+
+                [".."] = "I",
+                [".---"] = "J",
+                ["-.-"] = "K",
+                [".-.."] = "L",
+
+                ["--"] = "M",
+                ["-."] = "N",
+                ["---"] = "O",
+                [".--."] = "P",
+
+                ["--.-"] = "Q",
+                [".-."] = "R",
+                ["..."] = "S",
+                ["-"] = "T",
+
+                ["..-"] = "U",
+                ["...-"] = "V",
+                [".--"] = "W",
+
+                ["-..-"] = "X",
+                ["-.--"] = "Y",
+                ["--.."] = "Z",
+
+                ["...---..."] = "SOS",
+
+                ["-.-.--"] = "!",
+                [".-.-.-"] = ".",
+                ["..--.."] = "?",
+                ["--..--"] = ",",
+                ["---..."] = ":"
+            };
+
+            string result = "";
+
+            foreach (var word in input.Split("   "))
+            {
+                foreach (var letter in word.Split(" "))
+                {
+                    result += _translation[letter];
+                }
+                result += " ";
+            }
+
+            return result.TrimEnd();
+        }
+    }
+}
